6a7468faf18fca30fa626239a05e16943f2349a1
Jean-Pierre AHONSU <akjeanpi@gmail.com>
2026-01-17 18:46:59 +0100

    Create events/page.tsx

---
+++ app/events/page.tsx
@@ -0,0 +1,9 @@
+import { cookies } from "next/headers";
+import HomeClient from "../components/HomeClient";
+
+export default async function Page() {
+  const cookieStore = await cookies();
+  const isAdmin = cookieStore.get("admin_auth")?.value === "1";
+
+  return <HomeClient showAdmin={isAdmin} />;
+}
