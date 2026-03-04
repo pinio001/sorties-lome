dc727ed6140c8087a705485ced581a5c4f90dc49
Jean-Pierre AHONSU <akjeanpi@gmail.com>
2026-03-04 20:07:20 +0100

    Add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,14 +3,20 @@ export default function sitemap() {
     {
       url: "https://sorties-lome.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
       url: "https://sorties-lome.vercel.app/events",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
     {
       url: "https://sorties-lome.vercel.app/places",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
